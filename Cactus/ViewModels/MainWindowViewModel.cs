f4981af93e856d851f66effe84ddc5653abc8d47
Jonathan Vasquez <jon@xyinn.org>
2018-05-18 19:13:35 -0400

    added license header to all files, removed unused references

--- Cactus/ViewModels/MainWindowViewModel.cs
+++ Cactus/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,22 @@
-﻿using Cactus.Interfaces;
+﻿// Copyright (C) 2018 Jonathan Vasquez <jon@xyinn.org>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Cactus.Interfaces;
 using Cactus.Models;
 using Cactus.Views;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 
@@ -30,7 +43,7 @@ namespace Cactus.ViewModels
         public RelayCommand LaunchCommand { get; private set; }
 
         private readonly string _appName = "Cactus";
-        private readonly string _version = "0.0.1";
+        private readonly string _version = "0.0.2";
 
         public MainWindowViewModel(IEntryManager entryManager, IFileSwitcher fileSwitcher, IEditWindowViewModel editWindowViewModel)
         {
